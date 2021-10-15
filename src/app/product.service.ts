93e2c2ec0049438e52edf5bfea841adb53624aca
BarasaK <nicholas.k.barasa@gmail.com>
2021-10-15 09:15:36 +0300

    rn1

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse  } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).map(HttpResponse) =>
+    HttpResponse.json());
+  }
+
+}
